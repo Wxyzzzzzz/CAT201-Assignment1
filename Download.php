a8263b611f36d4a88503dad46ff7465ffe70627f
jiajoo-alt <120643036+jiajoo-alt@users.noreply.github.com>
2023-12-10 20:42:55 +0800

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,27 +1,30 @@
 <?php
-// get converted file names from URL parameter
-$convertedFiles = explode(",", $_GET['converted_files']);
 
-// check if at least one converted file is provided
-if (!empty($convertedFiles)) {
-    foreach ($convertedFiles as $converted_file) {
-        // get path of converted file name
-        $location_output = 'outputs/' . $converted_file;
+// Set the directory to list files from
+$directory = "outputs/";
 
-        // if successfully get converted file name and path
-        if (!empty($converted_file) && file_exists($location_output)) {
-            header("Cache-Control: public");
-            header("content-Description: File Transfer");
-            header("content-Disposition: attachment; filename=$converted_file");
+// Check if the directory exists
+if (is_dir($directory)) {
+    $files = scandir($directory);
+    $files = array_diff($files, array('.', '..'));
 
-            // download converted file
-            readfile($location_output);
-        } else {
-            echo "One or more files do not exist.";
-            break; // exit the loop if any file is missing
+    // Output a simple HTML page with download links
+    echo "<html><head><title>Download Files</title></head><body>";
+    echo "<h2>Files available for download:</h2>";
+
+    if (empty($files)) {
+        echo "No files found.";
+    } else {
+        echo "<ul>";
+        foreach ($files as $file) {
+            echo '<li><a href="test.php?file=' . urlencode($file) . '">' . $file . '</a></li>';
         }
+        echo "</ul>";
     }
+
+    echo "</body></html>";
 } else {
-    echo "No valid converted files specified or files do not exist.";
+    echo "Invalid directory.";
 }
+
 ?>
