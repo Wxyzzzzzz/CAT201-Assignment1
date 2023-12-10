22c27593dcff12c3e2a44ca18db893b8c99121f2
jiajoo-alt <120643036+jiajoo-alt@users.noreply.github.com>
2023-12-10 18:48:03 +0800

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,20 +1,27 @@
 <?php
-// get converted  file name from url parameter
-$converted_file = $_GET['converted_file'];
+// get converted file names from URL parameter
+$convertedFiles = explode(",", $_GET['converted_files']);
 
-// get path of converted file name
-$location_output= 'outputs/'. $converted_file;
+// check if at least one converted file is provided
+if (!empty($convertedFiles)) {
+    foreach ($convertedFiles as $converted_file) {
+        // get path of converted file name
+        $location_output = 'outputs/' . $converted_file;
 
-// if successfully get converted file name and path
-if (!empty($converted_file) && file_exists($location_output)){
-header("Cache-Control: public");
-header("content-Description: File Transfer");
-header("content-Disposition: attachment; filename={$converted_file}");
+        // if successfully get converted file name and path
+        if (!empty($converted_file) && file_exists($location_output)) {
+            header("Cache-Control: public");
+            header("content-Description: File Transfer");
+            header("content-Disposition: attachment; filename=$converted_file");
 
-//download converted file
-readfile($location_output);
+            // download converted file
+            readfile($location_output);
+        } else {
+            echo "One or more files do not exist.";
+            break; // exit the loop if any file is missing
+        }
+    }
+} else {
+    echo "No valid converted files specified or files do not exist.";
 }
-
-else {
-    echo "This file does not exist.";
-}
+?>
