c14bcf8cb9101581364f7b4e58cc11037e56ec40
jiajoo-alt <120643036+jiajoo-alt@users.noreply.github.com>
2023-12-10 18:49:18 +0800

    Update Upload4.php

--- Upload4.php
+++ Upload4.php
@@ -14,49 +14,55 @@ $allowedExtensions = ['txt', 'pdf'];
 $successfulUploads = [];
 $invalidUploads = [];
 
-$names = $_FILES['image']['name'];
-$tmp_names = $_FILES['image']['tmp_name'];
+// Process each uploaded file
+foreach ($_FILES['image']['name'] as $key => $filename) {
+    $tmp_name = $_FILES['image']['tmp_name'][$key];
 
-$upload_data = array_combine($tmp_names, $names);
-
-foreach ($upload_data as $temp_folder => $file) {
-    $pathinfo = pathinfo($file);
+    $pathinfo = pathinfo($filename);
     $fileExtension = strtolower($pathinfo['extension']);
 
     // Check if the file has a valid extension
     if (!in_array($fileExtension, $allowedExtensions)) {
-        $invalidUploads[] = $file;
+        $invalidUploads[] = $filename;
     } else {
-        //  Replace any characters not \w- in the original filename
+        // Replace any characters not \w- in the original filename
         $base = $pathinfo["filename"];
         $base = preg_replace("/[^\w-]/", "_", $base);
-        $filename = $base . "." . $pathinfo["extension"];
+        $newFilename = $base . "." . $fileExtension;
 
-        // // Add a numeric suffix if the file already exists
+        // Add a numeric suffix if the file already exists
         $i = 1;
-        $upload_file_tmp_name = __DIR__ . "/uploads/" . $filename;
+        $upload_file_tmp_name = __DIR__ . "/uploads/" . $newFilename;
         while (file_exists($upload_file_tmp_name)) {
-            $filename = $base . "($i)." . $pathinfo["extension"];
-            $upload_file_tmp_name = __DIR__ . "/uploads/" . $filename;
+            $newFilename = $base . "($i)." . $fileExtension;
+            $upload_file_tmp_name = __DIR__ . "/uploads/" . $newFilename;
             $i++;
         }
 
-        // Track the successful upload
-        $successfulUploads[] = $filename;
-
         // Move the file with edited filename
-        move_uploaded_file($temp_folder, $upload_file_tmp_name);
+        if (move_uploaded_file($tmp_name, $upload_file_tmp_name)) {
+            $successfulUploads[] = $newFilename;
+        }
     }
 }
 
 // Provide feedback to the user
 
 if (!empty($successfulUploads)) {
-    echo 'Successfully uploaded files:<br>';
+    echo 'Successfully uploaded files to server:<br>';
     
     foreach ($successfulUploads as $uploadedFile) {
         echo $uploadedFile . '<br>';
     }
+
+    // Display the convert button
+    echo '<form method="post" action="convert6.php">';
+    foreach ($successfulUploads as $uploadedFile) {
+        echo '<input type="hidden" name="uploaded_files[]" value="' . htmlspecialchars($uploadedFile) . '">';
+    }
+    echo '<br>';
+    echo '<button type="submit" name="convert">Convert</button>';
+    echo '</form>';
 }
 
 if (!empty($invalidUploads)) {
@@ -66,18 +72,4 @@ if (!empty($invalidUploads)) {
         echo $invalidFile . '<br>';
     }
 }
-
-?>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        // Display the convert button
-        echo '<form method="post" enctype="multipart/form-data" action="convert6.php">';
-        foreach ($_FILES['image']['name'] as $uploadedFile) {
-            echo '<input type="hidden" name="uploaded_file[]" value="' . htmlspecialchars($uploadedFile) . '">';
-        }
-        echo '<br>';
-        echo '<button type="submit" name="convert">Convert</button>';
-        echo '</form>';
-    }
 ?>
